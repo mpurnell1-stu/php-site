e75105f6662fa65b3941a70c5bb653f785a43182
Matthew Purnell <mpurnell1@stu.parkland.edu>
2021-03-08 13:38:20 -0600

    Added templates for all pages

---
+++ 2by2.php
@@ -0,0 +1,19 @@
+<!--  I honor Parkland's core values by affirming that I have
+followed all academic integrity guidelines for this work.
+Matt Purnell
+CSC 155-201F -->
+<!DOCTYPE html>
+<html lang='en-us'>
+<head>
+    <meta charset='UTF-8'>
+    <title>Item 1</title>
+    <?php
+        require('lib/includes.php');
+    ?>
+</head>
+<body>
+    <?php insert_header() ?>
+    <p>This page will be an 'item' page.</p>
+    <?php insert_footer() ?>
+</body>
+</html>
