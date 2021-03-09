3d7d45845ec8bac8da41d038dd808f289825e935
Matthew Purnell <mpurnell1@stu.parkland.edu>
2021-03-08 23:50:58 -0600

    Added cart and display functions

--- cart.php
+++ cart.php
@@ -9,11 +9,62 @@ CSC 155-201F -->
     <title>Shopping Cart</title>
     <?php
         require('lib/includes.php');
+        function display_cart() {
+            $ret = '';
+            if (isset($_SESSION['cart'])) {
+                $cart = $_SESSION['cart'];
+                if ($cart['2by2'] == 1) {
+                    $ret .= 'one 2x2, ';
+                }
+                else if ($cart['2by2'] >= 2) {
+                    $ret .= $cart['2by2'] . " 2x2's, ";
+                }
+                if ($cart['3by3'] == 1) {
+                     $ret .= 'one 3x3, ';
+                }
+                else if ($cart['3by3'] >= 2) {
+                    $ret .= $cart['3by3'] . "3x3's, ";
+                }
+                if ($cart['4by4'] == 1) {
+                    $ret .= 'one 4x4, ';
+                }
+                else if ($cart['4by4'] >= 2) {
+                    $ret .= $cart['4by4'] . " 4x4's, ";
+                }
+                if ($cart['5by5'] == 1) {
+                    $ret .= 'one 5x5';
+                }
+                else if ($cart['5by5'] >= 2) {
+                    $ret .= $cart['5by5'] . " 5x5's";
+                }
+            }
+            if ($ret == false) {
+                $ret = 'nothing';
+            }
+            else if (substr($ret, -2) == ', ') {
+                $ret = substr($ret, 0, strlen($ret) - 2 - 1);
+            }
+            return $ret;
+        }
+        function display_total() {
+            if (isset($_SESSION['cart'])) {
+                $cart = $_SESSION['cart'];
+                return ((5 * $cart['2by2']) + (15 * $cart['3by3'])
+                        + (30 * $cart['4by4']) + (50 * $cart['5by5']));
+            }
+            else {
+                return '0.00';
+            }
+        }
     ?>
 </head>
 <body>
     <?php insert_header() ?>
-    <p>This page will be the shopping cart page.</p>
+    <p>
+        This is your shopping cart. While browsing this site you have added
+        <?php echo display_cart() ?> to your cart. This will cost a total of
+        $<?php echo display_total() ?>.
+    </p>
     <?php insert_footer() ?>
 </body>
 </html>
